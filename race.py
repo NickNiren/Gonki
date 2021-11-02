f52f30c5c874adcdcd5599c3e98cc5bf3e6e172d
viaeung <93202163+viaeung@users.noreply.github.com>
2021-11-02 16:29:42 -0400

    Update race.py

--- race.py
+++ race.py
@@ -4,10 +4,10 @@ screen = pygame.display.set_mode((750, 1000))
 pygame.display.set_caption("Gonki")
 screen.fill(background_color)
 pygame.display.flip()
-
+car = pygame.image.load(r'C:\Users\E\Documents\GitHub\Gonki\car.png')
 running = True
 while running:
-    
+    display_surface.blit(image, (0,0))
     for event in pygame.event.get():   
         if event.type == pygame.QUIT:
             running = False
