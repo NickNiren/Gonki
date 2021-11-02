13a57b49ddd8254a108669e5e0cb1fc53cdbcc48
NickNiren <93608547+NickNiren@users.noreply.github.com>
2021-11-02 16:16:09 -0400

    Update race.py

--- race.py
+++ race.py
@@ -1,1 +1,13 @@
-print("this is text")
+import pygame
+background_color = (0,130,23)
+screen = pygame.display.set_mode((750, 1000))
+pygame.display.set_caption("Gonki")
+screen.fill(background_color)
+pygame.display.flip()
+
+running = True
+while running:
+    
+    for event in pygame.event.get():   
+        if event.type == pygame.QUIT:
+            running = False
