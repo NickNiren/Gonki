ab5d7a42684b6d1a5d96ecfb15f1b2ca5cd0cf92
NickNiren <93608547+NickNiren@users.noreply.github.com>
2021-11-02 16:51:26 -0400

    added car and car movement

--- race.py
+++ race.py
@@ -4,10 +4,29 @@ screen = pygame.display.set_mode((750, 1000))
 pygame.display.set_caption("Gonki")
 screen.fill(background_color)
 pygame.display.flip()
-car = pygame.image.load(r'C:\Users\E\Documents\GitHub\Gonki\car.png')
+#car
+car = pygame.Surface((50,100))
+playerX = 300
+carImage = pygame.image.load("car.png")
+carBox = pygame.Rect(0, 0, 50, 100)
+#main loop
 running = True
 while running:
-    display_surface.blit(image, (0,0))
+    screen.fill(background_color)
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_a]:
+            playerX -= .5
+    if keys[pygame.K_d]:
+            playerX += .5
+
     for event in pygame.event.get():   
         if event.type == pygame.QUIT:
             running = False
+    car.blit(carImage, (0,0))
+    car.blit(carBox, (0,0))
+    screen.blit(car, (playerX,650))
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 700:
+        playerX = 700
+    pygame.display.update()
