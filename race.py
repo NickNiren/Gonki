3637be3dd4a18d94df79a6eaebffa6834e571ac0
NickNiren <93608547+NickNiren@users.noreply.github.com>
2021-11-04 14:35:16 -0400

    Update race.py
    added rock movement

--- race.py
+++ race.py
@@ -1,6 +1,11 @@
 import pygame
+import time
+import random
+import threading
+reroll = True
+
 background_color = (0,130,23)
-screen = pygame.display.set_mode((750, 1000))
+screen = pygame.display.set_mode((400, 600))
 pygame.display.set_caption("Gonki")
 screen.fill(background_color)
 pygame.display.flip()
@@ -9,24 +14,48 @@ car = pygame.Surface((50,100))
 playerX = 300
 carImage = pygame.image.load("car.png")
 carBox = pygame.Rect(0, 0, 50, 100)
+#rocks
+class Rocks:
+    rock = pygame.Surface(((50,50)), pygame.SRCALPHA)
+    rockBox = pygame.draw.circle(rock, (128,128,128), (25,25), 25)
+    def __init__(self):       
+        self.rockX = random.randint(25,375)
+        self.rockY = random.randint(25,200)    
+def initRocks():
+    rock1 = Rocks()
+    rock2 = Rocks()
+    rock3 = Rocks()
+    rockList = [rock1, rock2, rock3]
+    global reroll
+    reroll = False
+    return rockList
+def moveRock(rockList):
+    #this function sucks, but we'll fix it later (maybe)
+    screen.blit(rockList[0].rock, (rockList[0].rockX, rockList[0].rockY))
+    screen.blit(rockList[1].rock, (rockList[1].rockX, rockList[1].rockY))
+    screen.blit(rockList[2].rock, (rockList[2].rockX, rockList[2].rockY))
+    for x in rockList:
+        x.rockY += 0.1
 #main loop
 running = True
 while running:
     screen.fill(background_color)
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a]:
-            playerX -= .5
+            playerX -= .4
     if keys[pygame.K_d]:
-            playerX += .5
+            playerX += .4
 
     for event in pygame.event.get():   
         if event.type == pygame.QUIT:
             running = False
     car.blit(carImage, (0,0))
-    car.blit(carBox, (0,0))
-    screen.blit(car, (playerX,650))
+    screen.blit(car, (playerX,300))
+    if reroll == True:
+        rockList = initRocks()
+    moveRock(rockList)
     if playerX <= 0:
         playerX = 0
-    elif playerX >= 700:
-        playerX = 700
+    elif playerX >= 350:
+        playerX = 350
     pygame.display.update()
