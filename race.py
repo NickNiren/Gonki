17441e2d5095ad814d8c9a6179a8f60b39c2b33e
NickNiren <93608547+NickNiren@users.noreply.github.com>
2021-11-05 13:36:49 -0400

    Update race.py

--- race.py
+++ race.py
@@ -3,7 +3,6 @@ import time
 import random
 import threading
 import sys
-reroll = True
 
 background_color = (0,130,23)
 screen = pygame.display.set_mode((400, 600))
@@ -27,27 +26,26 @@ def initRocks():
     rock2 = Rocks()
     rock3 = Rocks()
     rockList = [rock1, rock2, rock3]
-    global reroll
-    reroll = False
     return rockList
 def moveRock(rockList):
     #this function sucks, but we'll fix it later (maybe)
-    for y in range(2000):
-        screen.blit(rockList[0].rock, (rockList[0].rockX, rockList[0].rockY))
-        screen.blit(rockList[1].rock, (rockList[1].rockX, rockList[1].rockY))
-        screen.blit(rockList[2].rock, (rockList[2].rockX, rockList[2].rockY))
-        for x in rockList:
-            x.rockY += 0.1
-def rockTimer(end):
-  tim = threading.Timer(3.0,rockTimer(False)).start()
-  print("poopoo")
-  if end == True:
-      tim.cancel()
-rockTimer(False)
+    tim2 = threading.Timer(0.1, moveRock, [rockList])
+    tim2.start()
+    screen.blit(rockList[0].rock, (rockList[0].rockX, rockList[0].rockY))
+    screen.blit(rockList[1].rock, (rockList[1].rockX, rockList[1].rockY))
+    screen.blit(rockList[2].rock, (rockList[2].rockX, rockList[2].rockY))
+    for x in rockList:
+        x.rockY += 0.1
+def rockTimer():
+    tim = threading.Timer(10.0, rockTimer)
+    tim.start()
+    print("Calling MoveRock")
+    moveRock(initRocks())
+rockTimer()
 #main loop
 running = True
 while running:
-    screen.fill(background_color)
+    #screen.fill(background_color)
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a]:
             playerX -= .4
@@ -56,7 +54,7 @@ while running:
     for event in pygame.event.get():   
         if event.type == pygame.QUIT:
             running = False
-            rockTimer(True)
+            #rockTimer(True)
             pygame.display.quit()
             sys.exit()        
     car.blit(carImage, (0,0))
