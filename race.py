95b1c89fd16b56c6e6d7ba0cbb9eb083590364ac
NickNiren <93608547+NickNiren@users.noreply.github.com>
2021-11-05 12:56:12 -0400

    Update race.py

--- race.py
+++ race.py
@@ -2,6 +2,7 @@ import pygame
 import time
 import random
 import threading
+import sys
 reroll = True
 
 background_color = (0,130,23)
@@ -31,11 +32,18 @@ def initRocks():
     return rockList
 def moveRock(rockList):
     #this function sucks, but we'll fix it later (maybe)
-    screen.blit(rockList[0].rock, (rockList[0].rockX, rockList[0].rockY))
-    screen.blit(rockList[1].rock, (rockList[1].rockX, rockList[1].rockY))
-    screen.blit(rockList[2].rock, (rockList[2].rockX, rockList[2].rockY))
-    for x in rockList:
-        x.rockY += 0.1
+    for y in range(2000):
+        screen.blit(rockList[0].rock, (rockList[0].rockX, rockList[0].rockY))
+        screen.blit(rockList[1].rock, (rockList[1].rockX, rockList[1].rockY))
+        screen.blit(rockList[2].rock, (rockList[2].rockX, rockList[2].rockY))
+        for x in rockList:
+            x.rockY += 0.1
+def rockTimer(end):
+  tim = threading.Timer(3.0,rockTimer(False)).start()
+  print("poopoo")
+  if end == True:
+      tim.cancel()
+rockTimer(False)
 #main loop
 running = True
 while running:
@@ -45,15 +53,14 @@ while running:
             playerX -= .4
     if keys[pygame.K_d]:
             playerX += .4
-
     for event in pygame.event.get():   
         if event.type == pygame.QUIT:
             running = False
+            rockTimer(True)
+            pygame.display.quit()
+            sys.exit()        
     car.blit(carImage, (0,0))
     screen.blit(car, (playerX,300))
-    if reroll == True:
-        rockList = initRocks()
-    moveRock(rockList)
     if playerX <= 0:
         playerX = 0
     elif playerX >= 350:
